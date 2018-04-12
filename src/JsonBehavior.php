dc4e0f5646ac3f52758e8b72e9f6f9d3f09d1a3d
Sergey Klimenchuk <ksl1980@mail.ru>
2018-04-12 12:48:53 +0300

    init

---
+++ src/JsonBehavior.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @link https://klisl.com
+ * @copyright Klimenchuk Sergey
+ */
+
+namespace klisl\behaviors;
+
+use yii\base\Behavior;
+use yii\base\Event;
+use yii\db\ActiveRecord;
+use yii\helpers\Json;
+
+/**
+ * Поведение для автоматической конвертации свойств объектов ActiveRecord в формат JSON и обратно
+ * при сохранении данных в БД и при получении данных из нее.
+ *
+ * Подключение в объекте ActiveRecord:
+ * public function behaviors(): array
+ * {
+ *  return [
+ *      [
+ *          'class' => JsonBehavior::class,
+ *          'property' => 'meta',
+ *          'jsonField' => 'meta_json'
+ *      ]
+ *  ];
+ * }
+ * Если свойство объекта имеет название идентичное полю в БД, атрибут 'jsonField' можно не указывать.
+ *
+ * @property string $property Свойство содержащее объект или массив до конвертации в формат JSON
+ * @property string $jsonField Поле таблицы для хранения данных в формате JSON
+ */
+class JsonBehavior extends Behavior
+{
+    public $property;
+    public $jsonField;
+
+    /**
+     * Список событий на которые зарегистрировано выполнение указанных методов
+     * @return array
+     */
+    public function events(): array
+    {
+        return [
+            ActiveRecord::EVENT_AFTER_FIND => 'onAfterFind',
+            ActiveRecord::EVENT_BEFORE_INSERT => 'onBeforeSave',
+            ActiveRecord::EVENT_BEFORE_UPDATE => 'onBeforeSave',
+        ];
+    }
+
+    public function onAfterFind(Event $event): void
+    {
+        /** @var ActiveRecord $model */
+        $model = $event->sender;
+        $jsonField = $this->getJsonField($model);
+
+        $model->{$this->property} = Json::decode($model->getAttribute($jsonField));
+    }
+
+    public function onBeforeSave(Event $event): void
+    {
+        /** @var ActiveRecord $model */
+        $model = $event->sender;
+        $jsonField = $this->getJsonField($model);
+
+        $model->setAttribute($jsonField, Json::encode($model->{$this->property}));
+    }
+
+    protected function getJsonField(ActiveRecord $model): string
+    {
+        $jsonField = $this->jsonField ?? $this->property;
+
+        if (!$model->hasAttribute($jsonField)){
+            throw new \DomainException("Field $jsonField with type JSON does not exist in the table " . $model::tableName());
+        }
+        return $jsonField;
+    }
+}
